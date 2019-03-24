536b1a02583e4edf53cdcac9920675781fb66624
sungpyo9053 <44603374+sungpyo9053@users.noreply.github.com>
2019-03-24 21:45:47 +0900

    Create ArrayException.java

---
+++ ArrayException.java
@@ -0,0 +1,19 @@
+package kr.ac.kookmin.cs.opp.ch5;
+
+public class ArrayException {
+
+	public static void main(String[] args) {
+		int[] intArray = new int[5];
+		intArray[0] =0;
+		try {
+			for (int i = 0; i<5; i++) {
+				intArray[i+1] = i+1 +intArray[i];
+				System.out.println("intArray["+i+"]"+"=" +intArray[i]);
+			}
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("배열의 인덱스가 범위를 벗어났습니다.");
+			System.out.println(e);
+		}
+	}
+}
