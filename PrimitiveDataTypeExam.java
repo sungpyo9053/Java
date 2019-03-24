4a8988fa94a8a7b0c051c4205b0fe6c5a79142a9
sungpyo9053 <44603374+sungpyo9053@users.noreply.github.com>
2019-03-24 21:49:20 +0900

    Create PrimitiveDataTypeExam.java

---
+++ PrimitiveDataTypeExam.java
@@ -0,0 +1,20 @@
+package kr.ac.kookmin.cs.oop.ch2;
+
+public class PrimitiveDataTypeExam {
+
+	public static void main(String[] args) {	
+		boolean isFun = true;
+		System.out.println(isFun);
+		
+		
+		char c = 'f'; // 문자 하나만 선언가능
+		
+		int x = 59; //값 자체를 리터럴 이라고하더라
+		long bing = 32421245L; // 롱타입이라는 끝에 L 을 넣어줘야함
+		
+		float f = 32.4F; //실수표시
+		
+		double d = 31214234.1; //조금더 큰 수
+	}
+
+}
